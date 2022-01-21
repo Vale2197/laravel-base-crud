1a60e1cabbd5338f17ad05a5a629d04ed9a67662
Vale2197 <valentinatrebbi97@gmail.com>
2022-01-21 19:32:08 +0100

    creato modello game, migration e rotte

---
+++ app/Models/Game.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    //
+    protected $fillable = ['title', 'image', 'description'];
+    protected $table = 'games';
+}
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -10,7 +10,7 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+        <script src="{{asset('js/app.js')}}" defer></script>
         <!-- Styles -->
     </head>
     <body>
@@ -21,5 +21,6 @@
        </main>
 
        @include('partials.footer')
+
     </body>
 </html>
